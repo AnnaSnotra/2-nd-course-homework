c36d6f6ab6263ae3b9913b9386e7e68f93d19114
AnnaSnotra <ania.demicheva@gmail.com>
2023-07-05 10:35:28 +0300

    hw3

--- Homework3/hw3.js
+++ Homework3/hw3.js
@@ -1,8 +1,8 @@
 
 // Задание 1
 
-let password = String(prompt('Введите пароль'));
-password = password.toLocaleLowerCase();
+let password = prompt('Введите пароль');
+password = 'заяц';
 if (password === 'заяц') {
     alert('Пароль введен верно');
 } else {
@@ -40,41 +40,25 @@ alert(Number(a) + Number(b));
 let monthNumber = prompt('Введите номер месяца');
 
 switch (monthNumber) {
+    case '12':
     case '1':
-        console.log('зима');
-        break;
     case '2':
         console.log('зима');
         break;
     case '3':
-        console.log('весна');
-        break;
     case '4':
-        console.log('весна');
-        break;
     case '5':
         console.log('весна');
         break;
     case '6':
-        console.log('лето');
-        break;
     case '7':
-        console.log('лето');
-        break;
     case '8':
         console.log('лето');
         break;
     case '9':
-        console.log('осень');
-        break;
     case '10':
-        console.log('осень');
-        break;
     case '11':
         console.log('осень');
-        break;
-    case '12':
-        console.log('зима');
         break;
 
     default:
